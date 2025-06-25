5d475b16b45b6dd5636aa193fcf311d4a785a22c
Arthurnacious <arthurnacious@gmail.com>
2025-06-25 17:57:27 +0200

    dynamic routes implementation

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -54,9 +54,8 @@ class Router
 
         $path = $request->getPathInfo();
 
-        if (!str_ends_with($path, '/')) {
-            $path .= '/';
-        }
+        // Remove trailing slash for dynamic routes
+        $path = rtrim($path, '/');
 
         $routeInfo = $this->dispatcher->dispatch(
             $request->getMethod(),
@@ -64,23 +63,68 @@ class Router
         );
 
         return match ($routeInfo[0]) {
-            Dispatcher::NOT_FOUND => new Response('Not Found', 404),
+            Dispatcher::NOT_FOUND => $this->handleNotFound($path),
             Dispatcher::METHOD_NOT_ALLOWED => new Response('Method Not Allowed', 405),
-            Dispatcher::FOUND => $this->handleFoundRoute($routeInfo),
+            Dispatcher::FOUND => $this->handleFoundRoute($routeInfo, $request),
             default => new Response('Server Error', 500)
         };
     }
 
-    private function handleFoundRoute(array $routeInfo): Response
+    private function handleNotFound(string $path): Response
+    {
+        // Try again with trailing slash if not found
+        $retryPath = $path . '/';
+        $retryInfo = $this->dispatcher->dispatch(
+            $_SERVER['REQUEST_METHOD'] ?? 'GET',
+            $retryPath
+        );
+
+        if ($retryInfo[0] === Dispatcher::FOUND) {
+            return $this->handleFoundRoute($retryInfo, $request);
+        }
+
+        return new Response('Not Found', 404);
+    }
+
+    private function handleFoundRoute(array $routeInfo, Request $request): Response
     {
         try {
             [$controllerClass, $methodName] = $routeInfo[1];
             $params = $routeInfo[2];
 
             $controller = $this->container->get($controllerClass);
-            return $controller->$methodName(...array_values($params));
+            $method = new \ReflectionMethod($controller, $methodName);
+
+            $args = [];
+            foreach ($method->getParameters() as $param) {
+                $paramName = $param->getName();
+                $paramType = $param->getType();
+
+                if (array_key_exists($paramName, $params)) {
+                    // Route parameter
+                    $args[] = $this->castParameter($params[$paramName], $paramType);
+                } elseif ($paramType && is_a($paramType->getName(), Request::class, true)) {
+                    // Request object injection
+                    $args[] = $request;
+                } else {
+                    // Service injection
+                    $args[] = $this->container->get($paramType->getName());
+                }
+            }
+
+            return $method->invokeArgs($controller, $args);
         } catch (\Throwable $e) {
             return new Response('Server Error: ' . $e->getMessage(), 500);
         }
+    }
+
+    private function castParameter($value, ?\ReflectionType $type)
+    {
+        if (!$type instanceof \ReflectionNamedType || $type->isBuiltin()) {
+            return $value;
+        }
+
+        settype($value, $type->getName());
+        return $value;
     }
 }
--- src/Modules/Hello/Controllers/HelloController.php
+++ src/Modules/Hello/Controllers/HelloController.php
@@ -14,4 +14,28 @@ class HelloController
     {
         return new Response('Hello from V8!');
     }
+
+    #[HttpMethod('/{name}', method: HttpMethod::GET)]
+    public function greet(string $name): Response
+    {
+        return new Response("Hello, $name!");
+    }
+
+    #[HttpMethod('/create', method: HttpMethod::POST)]
+    public function create(): Response
+    {
+        return new Response('Item created!', 201);
+    }
+
+    #[HttpMethod('/{id}', method: HttpMethod::PUT)]
+    public function update(string $id): Response
+    {
+        return new Response("Item $id updated!");
+    }
+
+    #[HttpMethod('/{id}', method: HttpMethod::DELETE)]
+    public function delete(string $id): Response
+    {
+        return new Response("Item $id deleted!", 204);
+    }
 }
