c9c747fb8b210e6172dfa5957fde0780b4fa3407
Arthurnacious <arthurnacious@gmail.com>
2025-06-26 20:30:58 +0200

    removed redundant comments

--- src/Core/Exception/ValidationException.php
+++ src/Core/Exception/ValidationException.php
@@ -12,10 +12,8 @@ class ValidationException extends InvalidArgumentException
     {
         $this->errors = $errors;
 
-        // Set the HTTP status code to 422 immediately
         http_response_code(422);
 
-        // Output the response and exit
         echo json_encode([
             'message' => $message,
             'errors' => $errors,
