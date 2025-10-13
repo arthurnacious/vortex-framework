42e29265613fcf3cfc11b738defe9b4c0ad1165c
Arthurnacious Monethi <arthurnacious@gmail.com>
2025-10-13 21:14:20 +0200

    reconfigured composer.json for minimal packages in the beginning

--- public/index.php
+++ public/index.php
@@ -2,5 +2,7 @@
 
 declare(strict_types=1);
 
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
 echo "Hyperdrive starting...\n";
-echo "Front controller working!\n";
+echo "Composer autoloader loaded!\n";
